310a863e44af8212f90fa20a4f9792d635505a99
Relais4x100a2 <guillaume.cayeux@dgcl.fr>
2022-08-03 23:24:08 +0200

    - JSON des événements individuels bien avancé - avancement site web

--- main.py
+++ main.py
@@ -2,33 +2,54 @@ import re
 import uuid
 
 
-def individu_parse():
+def parse():
     """
     Parse la chaîne mise en paramètre et retourne un dictionnaire
     :param
     :return: dict
     """
-    with open('pierre.txt', 'rt') as myfile:
-        fichier = myfile.read()
-
+    # VARIABLES
     # Listes vides destinées à recevoir l'ensemle des informations parsées.
     list_ind = []
+    list_ind_note = []
+    list_ind_source = []
+
     list_evt_ind = []
     list_evt_ind_note = []
     list_evt_ind_source = []
 
+    list_fam = []
+    list_fam_note = []
+    list_fam_source = []
+
+    list_evt_fam = []
+    list_evt_fam_note = []
+    list_evt_fam_source = []
+
+    list_relations = []
+
     # ***REGEX***
+
+    # regex pour les familles
+    regex_famille = r"(?s)(?<=^fam )(.*?)(?=^end\n)"
+
     # regex pour les individus
     regex_individu = r"(?s)(?<=^pevt )(.*?)(?=end pevt)"
     # regex pour les événements
     # fin de la regex (?=^#) pour inclure les notes liées à l'événement
     regex_evt = r"(?s)(?<=^#)(.*?)(?=^#)"
 
-    # On récupère toutes les concordances avec la regex pour les individus
-    matches = re.findall(regex_individu, fichier, re.MULTILINE)
+    # Prévoir étape pour renommer le ficier *.gw en *.txt
 
-    for m in matches:
-        # on crée la liste des individus, un individu étant défini dans un dictionnaire
+    # on lit le ficier *.gw converti en *.txt
+    with open('pierre.txt', 'rt') as myfile:
+        fichier = myfile.read()
+
+    # On identifie tous les individus ayant des "PEVT" (personal event).
+    matches_ind = re.findall(regex_individu, fichier, re.MULTILINE)
+    # Via une première boucle, on crée une liste des individus,
+    # défini sous la forme d'un dictionnaire (id_geneweb + id uuid4)
+    for m in matches_ind:
         dict_individu = {}
         line = m.split()
         id_g = line[0] + " " + line[1]
@@ -36,76 +57,111 @@ def individu_parse():
         dict_individu["id_ind"] = uuid.uuid4().hex
         list_ind.append(dict_individu)
 
+    # Via une deuxième boucle, on récupère les événements et les notes, sources et témoins liées.
+    #### Il faudra modifier ind_evt en ajoutant le statut dans l'événement.
+    for m in matches_ind:
+        # On relie l'individu à celui dans la liste créée précédement
+        lines = m.split("\n")
+
+        ind_evt_s = {}
+        info_ind = lines[0].split()
+        ind = info_ind[0] + " " + info_ind[1]
+        for indidividu in list_ind:
+            if ind == indidividu["id_geneweb"]:
+                ind_evt_s = indidividu
+        ind_evt_sujet = [{"ind": ind_evt_s, "role": "sujet_objet"}]
+
         # On crée la liste des événéments de chaque individu, chaque événément étant défini par un dictionnaire
-        match_evt = re.findall(regex_evt, m, re.MULTILINE)
-        for evt in match_evt:
+        matches_ind_evt = re.findall(regex_evt, m, re.MULTILINE)
+        for evt in matches_ind_evt:
             dict_evt = {}
             line = evt.split()
-            #print(line)
             dict_evt["id_evt"] = uuid.uuid4().hex
+            # Type d'événement
             dict_evt["type"] = line[0]
+            # Date d'événement
             if line[1]:
                 dict_evt["date"] = line[1]
             else:
                 dict_evt["date"] = None
+            # Lieu de l'événment
             try:
                 if line[2] == '#p':
                     dict_evt["place"] = line[3]
-            except:
+            except IndexError:
                 pass
-            dict_evt["ind_evt"] = [{"ind": dict_individu, "role": "objet"}]
-            list_evt_ind.append(dict_evt)
 
-            #On récupère les sources.
-            for s in range(0,len(line)-1):
-                dict_source={}
+            # Les sources de l'événement
+            for s in range(0, len(line) - 1):
                 if line[s] in ["#s", "#bs", "#ds"]:
-                    dict_source["cible"] = [{"ind": dict_individu, "evt": dict_evt["id_evt"]}]
-                    dict_source["value_source"]= [line[s+1]]
-                    list_evt_ind_source.append(dict_source)
+                    dict_evt["source"] = [line[s + 1]]
 
-            # On récupère les infos (note/wit) relatives aux événements des individus
-            # pour cela on récupère les lignes de chaque evt.
-            lines = evt.split("\n")
-            liste_note = []
+            # On relie l'événement :
+            lines_evt = evt.split("\n")
+            list_wit = []
+            list_note = []
+            # à l'individu sujet_objet de l'événement
+            # au(x) éventuel(s) "wit"(s) de l'événement
 
-            for i in lines:
-                lines_note = i.startswith("note ")
-                if lines_note is True:
+            for i in lines_evt:
+                if i.startswith("wit"):
+                    a = i.split()
+                    wit = a[2] + " " + a[3]
+                    for i in list_ind:
+                        if wit == i["id_geneweb"]:
+                            wit_id = {"ind": i, "role": "wit"}
+                            list_wit.append(wit_id)
+
+                # au(x) éventuelle(s) notes de l'événement.
+                elif i.startswith("note "):
                     x = i.replace("note ", '')
                     if x != '':
-                        liste_note.append(x)
+                        list_note.append(x)
 
-            if liste_note:
-                dict_note = {'related_to_evt_id': dict_evt["id_evt"], 'note': liste_note}
-                list_evt_ind_note.append(dict_note)
+            if list_wit:
+                dict_evt["ind_evt"] = ind_evt_sujet + list_wit
+            else:
+                dict_evt["ind_evt"] = ind_evt_sujet
 
-            # On récupère les sources.
-            """
-            liste_source = []
-            for i in lines:
-                lines_source = i.startswith("#s")
-                lines_source_b = i.startswith("#bs")
-                lines_source_d = i.startswith("#ds")
-                if lines_source is True or lines_source_b is True or lines_source_d is True:
-                    liste_source.append(i)
+            if list_note:
+                dict_evt["note"] = list_note
 
-                if liste_source:
-                    dict_source = {'related_to_evt_id': dict_evt["id_evt"], 'source': liste_source}
-                    list_evt_ind_source.append(dict_source)
-             """
+            list_evt_ind.append(dict_evt)
 
-                # On récupère les témoins.
-                # Il faudra modifier ind_evt en ajoutant le statut dans l'événement, via une deuxieme boucle.
-                # lines_wit = i.startswith("wit")
-                # if lines_wit is True:
-                # print(i)
+    # On récupère toutes les concordances avec la regex pour les familles
+    matches = re.findall(regex_famille, fichier, re.MULTILINE)
 
-    #print(list_ind)
-    # print(list_evt_ind)
+    for m in matches:
+        # on crée la liste des familles, une famille étant définie par un couple d'individu avec éventuellement des
+        # enfants.
+        dict_organisation = {}
+        dict_membre_organisation = {}
+        lines = m.split("\n")
+        info_fam = lines[0].split()
+        M1 = info_fam[0] + " " + info_fam[1]
+        for i in list_ind:
+            if M1 == i["id_geneweb"]:
+                dict_membre_organisation["ind"] = i
+                dict_membre_organisation["role"] = "conjoint"
+                # print(dict_membre_organisation)
+
+        # line = m.split()
+        # id_g = line[0] + " " + line[1]
+        # dict_individu["id_geneweb"] = id_g
+        dict_organisation["type_org"] = "famille"
+        dict_organisation["id_org"] = uuid.uuid4().hex
+        dict_organisation["membres_org"] = []
+        # conjoint
+        # conjointe
+        # enfant -h -f)
+
+    # on assemble les listes individus, familles (=organisation)
+
+    # print(list_ind)
+    print(list_evt_ind)
     # print(list_evt_ind_note)
-    #print(list_evt_ind_source)
+    # print(list_evt_ind_source)
 
 
 if __name__ == '__main__':
-    individu_parse()
+    parse()
--- notes.md
+++ notes.md
@@ -1,7 +1,9 @@
 #### Pour améliorer le code, regarder
 https://github.com/MaximeChallon/AdresseParser/blob/master/AdresseParser/AdresseParser.py
-#### Pour MongoDB
-https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb
+
+#### Le modèle du fichier geneweb
+https://geneweb.tuxfamily.org/wiki/gw/fr
+
 #### Pour Flask
 * https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3-fr
 
@@ -11,8 +13,12 @@ https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb
 
 * suppression du fichier après parsage : https://stackoverflow.com/questions/24612366/delete-an-uploaded-file-after-downloading-it-from-flask
 
-#### Idées
-* Match d'une personne va se faire avec  nom/prénom + événements + organisation (on calculera un score) + notice d'autorité.
+#### Pour MongoDB
+https://www.mongodb.com/blog/post/getting-started-with-python-and-mongod
+
+#### Idées pour plus tard
+* Match d'une personne va se faire avec  nom/prénom 
+* + événemenpour plus tardts + organisation (on calculera un score) + notice d'autorité.
 
 * Proposer enregistrement fichier sur serveur (filtre:personne décédée avant 1922) et montrer correspondance.
 
@@ -24,8 +30,6 @@ lines_note = lines_note.readlines()
 print(lines_note)
 ```
 
-
-
 ```
 dict_evt = {
                 "id_evt": uuid.uuid4().hex,
@@ -34,6 +38,35 @@ dict_evt = {
                 "ind_evt": [{"ind": dict_individu, "role": "objet"}]
             }
 ```
+```
+<link rel="stylesheet" href="{{ url_for('static', filename= 'css/style.css') }}">
+```
 
 
-
+```
+# On récupère les témoins
+#match_evt = re.findall(regex_evt, m, re.MULTILINE)
+#for evt in match_evt:
+    lines_evt = evt.split("\n")
+    for i in lines_evt:
+        if i.startswith("wit"):
+            a = i.split()
+            wit = a[2] + " " + a[3]
+            for i in list_ind:
+                if wit == i["id_geneweb"]:
+                    ind_wit = i
+                    #Il faut relier le wit au bon événement
+                    #dict_evt["ind_evt"].append({"ind": ind_wit, "role": "wit"})
+                    #list_evt_ind.append(dict_evt)
+```
+```  
+# On récupère les sources.
+            for s in range(0, len(line) - 1):#### Pour MongoDB
+                dict_source = {}* https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb
+                if line[s] in ["#s", "#bs", "#ds"]:* https://pymongo.readthedocs.io/en/stable/examples/authentication.html
+                    #dict_source["type_cible"] = "evt_ind"
+                    #dict_source["cible"] = [{"ind": [dict_individu["id_ind"]], "evt": [dict_evt["id_evt"]]}]
+                    dict_source["value_source"] = [line[s + 1]]
+                    list_evt_ind_source.append(dict_source)
+```  
+     
