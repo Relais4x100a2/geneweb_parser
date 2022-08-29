11bc7f10d9c17b7601e5e27fa4be21a42b0229bc
Relais4x100a2 <guillaume.cayeux@dgcl.fr>
2022-08-29 02:36:26 +0200

    correction MVP

--- mysite/config.py
+++ mysite/config.py
@@ -10,23 +10,17 @@ class Config:
     """Configuration à partir des variables définies dans .env"""
 
     # Application settings
-
     APP_NAME = environ.get("APP_NAME")
+
 
     # Flask settings
     FLASK_APP = environ.get("FLASK_APP")
+    FLASK_DEBUG = environ.get("FLASK_DEBUG")
     FLASK_ENV = environ.get("FLASK_ENV")
-    #FLASK_DEBUG = False
     CSRF_ENABLED = True
-
-    # Flask-SQLAlchemy settings
     SECRET_KEY = environ.get("SECRET_KEY")
-
-    # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
-    SQLALCHEMY_POOL_RECYCLE = 250
-    SQLALCHEMY_ECHO = False
-
-    #UPLOAD
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024
+
+
+
+
